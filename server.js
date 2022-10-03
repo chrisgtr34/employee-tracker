9867e432927555ecfb535da4d324d7fefbdb1b2e
CHRIS <chris@CHRISs-MBP.attlocal.net>
2022-10-02 17:12:23 -0700

    adding prompt function

--- server.js
+++ server.js
@@ -13,3 +13,52 @@ const db = mysql.createConnection(
     },
     console.log(`Accessing employees database.`)
 );
+  
+// Starts prompt
+const startPrompt = () => {
+    inquirer.prompt ([
+        {
+        type: "list",
+        message: "Please select from the following",
+        name: "startPrompt",
+        choices: [
+            "View All Employees",
+            "Add Employee",
+            "Update Employee Role",
+            "View All Roles",
+            "Add Role",
+            "View All Departments",
+            "Add Department",
+            "Exit"]
+        }
+    ]).then(function(answer) {
+        switch (answer.startPrompt) {
+            case "View Employees":
+                showEmployees();
+                break;
+            case "Add Employee":
+                addEmployees();
+                break;
+            case "Update Employee Role":
+                updateEmployee();
+                break;
+            case "View All Roles":
+                viewRoles();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "View All Departments":
+                viewDepartments();
+                break;
+            case "Add Department":
+                addDepartment();
+                break;
+            case "Exit":
+                exit();
+                break;
+            }
+        })
+    };
+    
+
