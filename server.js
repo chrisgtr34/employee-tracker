8ca7af2e55d2aeb81a3c503261b1c22764d35fd1
CHRIS <chris@CHRISs-MBP.attlocal.net>
2022-10-02 17:17:55 -0700

    adding employee function

--- server.js
+++ server.js
@@ -62,3 +62,34 @@ const startPrompt = () => {
     };
     
 
+function showEmployees () {
+    const query = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT (manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+    db.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        startPrompt();
+    });
+}
+
+function addEmployees () {
+    inquirer.prompt ([
+        {
+            type: "input",
+            name: "firstName",
+            message: "Please enter employee's first name"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "Please enter employee's last name"
+        },
+        {
+            type: "input",
+            name: "roleId",
+            message: "Please enter employee's role ID"
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "Please enter employee's manager ID"
+        },
